1cd0272cc3875c22538b41e1a637ebfcbf80a8d9
nightleopard <win20011018@gmail.com>
2022-10-27 00:07:15 +0800

    PC6 clear

---
+++ 111-1PC6/Test.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC6
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] s_school = new string[3] {"台科", "北科", "亞東"};
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i_Ct = 0; i_Ct < s_school.Length; i_Ct++)
+            {
+                ListItem s_S.Text = s_S.Value = s_school[i_Ct];
+
+                ddl_Test.Items.Add(s_S);
+            }
+        }
+    }
+}
